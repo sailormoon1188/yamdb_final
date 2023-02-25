f8562ea894f4b0584b1d8c17ddfc5b06c7026097
Vandanova Mariya <handarkin@gmail.com>
2023-02-26 02:22:27 +0700

    web static2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -114,13 +114,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static/')),
+                    ('redoc', '/app/templates/')]
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 # User
 AUTH_USER_MODEL = 'users.User'
